af08dd9334f0b80faf1213ce6d929c6c2a298c76
randy.chen <myuirmb@gmail.com>
2016-07-29 11:53:38 +0800

    main

--- js/main-app.js
+++ js/main-app.js
@@ -1,6 +1,3 @@
-/**
- * Created by hsl on 16/6/28.
- */
 angular.module('mainApp', ["ui.router", "ngStorage", "pascalprecht.translate"]).config(['$translateProvider', function ($translateProvider) {
     var lang = window.localStorage.lang || 'cn';
     $translateProvider.preferredLanguage(lang);
@@ -17,156 +14,70 @@ angular.module("mainApp").filter("T", function ($translate) {
     };
 });
 
-angular.module('mainApp').controller('mainController', function ($scope, $localStorage, $http, $translate, $compile) {
+angular.module('mainApp').controller('mainController', function ($scope, $localStorage, $http, $translate, $compile, $interval) {
 
-    $scope.user = $localStorage.user;
+    $scope.theTime = new Date().toLocaleTimeString();
 
-    $scope.dcon = "";
-    $scope.widget = "";
-
-    $scope.includeUrl = ""; //node_modules/wg-ebs/wg-ebs.html
-    $scope.includeUrl1 = "";
-    $scope.includeUrl2 = "";
-    $scope.includeUrl3 = "";
-    $scope.includeUrl4 = "";
-    $scope.includeUrl5 = "";
-
-    $scope.show = function (name) {
-        $scope.includeUrl = "node_modules/" + name + "/" + name + ".html";
-    }
-
-    $scope.saveUser = function () {
-        $localStorage.user = $scope.newUser;
-        $scope.user = $localStorage.user;
-    }
-
-    function ShowDFWA(bshow) {
-        $scope.dfwa.setVisible(bshow);
-    }
-
-    function ShowDFWB(bshow) {
-        $scope.dfwb.setVisible(bshow);
-    }
-    function ShowDFWC(bshow) {
-        $scope.dfwc.setVisible(bshow);
-    }
-    function ShowDFWD(bshow) {
-        $scope.dfwd.setVisible(bshow);
-    }
-
-    function addX() {
-        $scope.dfwx = $scope.dfp.createDFPanel("聊天");
-        $scope.dfwx.setInitialLayoutReference($scope.dfwb);
-        /*
-                $scope.dfwx.addContentDiv(document.getElementById("dockfloatdivx"));
-        */
-        $scope.dfwx.initLayout(150, 300, 200, 300, DSXDFPanel.dockLeft);
-    }
-
-    $scope.switching = function (lang) {
-        $translate.use(lang);
-        window.localStorage.lang = lang;
-        //window.location.reload();
+    $scope.conf = function () {
+        if (confirm("确认退出系统吗？")) alert('确定');
     };
 
-
-    $scope.loads = 0;
-
-
     function init() {
-        $scope.loads = 2 + $scope.loads;
-        if ($scope.loads == 2) {
-
-            $scope.dfp = DSXDFUtil.createDSXDFUtil();
-            //$scope.dfp.loadStatesFromKey("LAYOUT_NOW");
-            $scope.dfp.addFixedPanel(document.getElementById("fixedontop"), DSXDFUtil.fixedTop);
-            $scope.dfp.addFixedPanel(document.getElementById("centerdiv"), DSXDFUtil.fixedCenter);
-            $scope.dfp.addFixedPanel(document.getElementById("fixedbottom"), DSXDFUtil.fixedBottom);
-
-            $http.get('/test/layout.json?' + Math.random()).success(function (data) {
-                var lay = data.layout[data.sel];
-                alert(lay.length);
-                for (var i = 0; i < lay.length; i++) {
-                    var div = document.createElement('div');
-                    div.style.overflow = 'auto';
-                    div.innerHTML = '<h1>哈哈</h1>我是新来的';
-
-                    alert(i);
-                    $scope['dfw' + i] = $scope.dfp.createDFPanel("title " + i);
-                    if (i == 0) {
-                        $scope['dfw' + i].addContentDiv(document.getElementById("msgdiv"));
-                    }
-                    else if (i == 1) {
-                        $scope['dfw' + i].setInitialLayoutReference($scope['dfw' + (i - 1)]);
-                        //$scope['dfw' + i].addContentDiv(div);
-                    }
-                    else {
-                        document.body.appendChild(div);
-                        $scope['dfw' + i].addContentDiv(div);
-                    }
-                    $scope['dfw' + i].initLayout(lay[i].left, lay[i].top, lay[i].width, lay[i].height, lay[i].position);
-                }
-            });
-
-
-
-        }
-
+        $('#tabcontent').css({ 'overflow-y': 'auto', 'overflow-x': 'hidden', 'padding': '5px', 'height': ($(window).height() - 146) + 'px' });
+        $("#top_msg,#bottom_msg").height(($(window).height() - 110) / 2);
+        $(".direct-chat-messages,.msg-body").css({ 'height': (($(window).height() - 245) / 2) + 'px' });
+        $interval(function () {
+            $scope.theTime = new Date().toLocaleTimeString();
+        }, 1000);
     }
     init();
 
-    $scope.newdialog = function () {
-        //alert(9876);
-        $("#dialog").dialog({ containment: "#centerdiv", scroll: false });
-        //$("#dialog").draggable({ containment: "#centerdiv", scroll: false });
+    $scope.addtab = function () {
+        var len = $('#tablabel>li').length;
+        $('#tablabel').append('<li><a data-toggle="tab" href="#tab' + (len + 1) + '" aria-expanded="true">tab' + (len + 1) + '</a></li>');
+        $('#tabcontent').append('<div class="row tab-pane" id="tab' + (len + 1) + '"></div>');
+    };
+    
+    $scope.addwg = function (flag,colm) {
+        var wg = {
+            "blank": "node_modules/wg-blank/wg-blank.html",
+            "price": "node_modules/wg-price/wg-price.html",
+            "order": "node_modules/wg-order/wg-order.html",
+            "query": "node_modules/wg-query/wg-query.html",
+            "log": "node_modules/wg-log/wg-log.html"
+        };
+        var html = '<div class="col-lg-' + colm + ' col-md-' + colm + ' col-sm-' + colm + '"><div ng-include="\'' + wg[flag] + '\'"></div></div>';
+        var tpl = $compile(html);
+        var e = tpl($scope);
+        $("#tabcontent>div[class*='active']").append(e);
+        //alert($("#tabcontent>div[class*='active']").html());
     };
 
-    $scope.addcont = function () {
-        //alert($scope.dcon);
-        $("#dialog" + $scope.dcon).dialog({ width: 800, height: 600 });
-    }
+    $scope.changetemp = function (f) {
+        //alert(f);
+        var wg = {
+            "blank": "node_modules/wg-blank/wg-blank.html",
+            "price": "node_modules/wg-price/wg-price.html",
+            "order": "node_modules/wg-order/wg-order.html",
+            "query": "node_modules/wg-query/wg-query.html",
+            "log": "node_modules/wg-log/wg-log.html"
+        };
+        $http.get('/js/temp.json?' + Math.random()).success(function (data) {
+            //alert(data[f][0]['name']);
+            var temp = data[f];
+            $('#tablabel,#tabcontent').empty();
+            for (var i = 0; i < temp.length; i++) {
+                $('#tablabel').append('<li><a data-toggle="tab" href="#tab' + (i + 1) + '" aria-expanded="true">' + temp[i].name + '</a></li>');
+                $('#tabcontent').append('<div class="row tab-pane" id="tab' + (i + 1) + '"></div>');
 
-    $scope.selwidget = function () {
-        //alert($scope.widget);
-        $scope['includeUrl' + $scope.dcon] = $scope.widget;
-    }
+                for (var j = 0; j < temp[i].wg.length; j++) {
+                    var html = '<div class="col-lg-' + temp[i].wg[j].col + ' col-md-' + temp[i].wg[j].col + ' col-sm-' + temp[i].wg[j].col + '"><div ng-include="\'' + wg[temp[i].wg[j].id] + '\'"></div></div>';
+                    var tpl = $compile(html);
+                    var e = tpl($scope);
+                    $("#tab" + (i + 1)).append(e);
+                }
+            }
+        });
+    };
 
-    $scope.test = function () {
-        //var tx = $(window.frames["ifr2"].document).find("input[id='txtx']").click();  //ie
-        //var setwg = $(document.getElementById('ifr2').contentWindow.document).find("input[id='wg']").val($scope.widget);
-        var txt = $(document.getElementById('ifr2').contentWindow.document).find("input[id='txtx']").click();   //all
-    }
-
-    $scope.divtab = function () {
-        alert('xxxxxx');
-
-        var tpl = $compile('<div id="draggable1" style="border:1px solid red; height:300px;width:300px;background:#f3f3f3;"><h3>全局 容器drag</h3><div ng-include="includeUrl1"></div></div>');
-        var e = tpl($scope);
-        e.draggable({ containment: "#ifr2", handle: "h3" });
-        $('#ifr2').append(e);
-
-        
-        //$element.append(e);
-        //$('#ifr2').html($compile('<div id="draggable1" style="border:1px solid red; height:300px;width:300px;background:#f3f3f3;"><h3>全局 容器drag</h3></div>'))($scope);
-    }
-
-    function loadDefaultLayout() {
-        init();
-    }
-    function loadSaveLayout() { }
-
-    function saveLayout() {
-        $scope.dfp.saveStatesIntoKey("LAYOUT_NOW");
-
-    }
-
-    $scope.ShowDFWA = ShowDFWA;
-    $scope.ShowDFWB = ShowDFWB;
-    $scope.ShowDFWC = ShowDFWC;
-    $scope.ShowDFWD = ShowDFWD;
-    $scope.loadDefaultLayout = loadDefaultLayout;
-    $scope.loadSaveLayout = loadSaveLayout;
-    $scope.saveLayout = saveLayout;
-    $scope.init = init;
-    $scope.addX = addX;
 });
